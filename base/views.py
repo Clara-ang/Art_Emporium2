ae8a9ff4d1cd3a330b9f3498e3b22f375bb873e4
Clara Lima <claralima601@gmail.com>
2022-10-18 14:02:40 -0300

    alt.

--- base/views.py
+++ base/views.py
@@ -4,6 +4,9 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 from django.urls import reverse_lazy
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
+from django.contrib.auth import login as login_django
+from django.contrib.auth.decorators import login_required
 
 
 
@@ -41,14 +44,30 @@ def register(request):
         email = request.POST.get('email')
         password = request.POST.get('password')
         
-        #user = User.objects.filter('username').first()
+        user = User.objects.filter(username = username).first()   
+        if user:
+            return render(request, "base/pages/permission.html")
         
-        #if user:
-        #    return render(request, "core/pages/permisson.html")
-        #else:
         user = User.objects.create_user(username=username, email=email, password=password)
+        user.save()
         
         return render(request, "base/pages/home.html")
+
+def login(request):
+    if request.method == 'GET':
+        return render(request, "base/pages/login.html")
+    else:
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        
+        user = authenticate(username=username, password=password)
+        
+        if user is not None:
+            login_django(request, user)
+            return render(request, "base/pages/personal.html") 
+            
+        else:
+           return render(request, "base/pages/register.html") 
 
 
 
